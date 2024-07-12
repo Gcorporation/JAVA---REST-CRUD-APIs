225083b056c127173c977a6df0d8312a913474ec
Prabhjot Singh <prabhjotsingh394@gmail.com>
2024-07-12 12:12:17 +0530

    Spring Boot REST Path Variables

--- src/main/java/com/gcorporation/demo/rest/StudentRestController.java
+++ src/main/java/com/gcorporation/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.gcorporation.demo.rest;
 
 import com.gcorporation.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +14,14 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" - return a list of students
+    private List<Student> theStudents;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    // define @PostConstruct to load the student data ... only once!
 
-        List<Student> theStudents = new ArrayList<>();
+    @PostConstruct
+    public void loadData() {
+
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Prabhjot", "Singh"));
 
@@ -26,7 +30,23 @@ public class StudentRestController {
         theStudents.add(new Student("Ghussa", "Singh"));
 
         theStudents.add(new Student("Bhindi", "Singh"));
+    }
+
+    // define endpoint for "/students" - return a list of students
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
 
         return theStudents;
+    }
+
+    // define endpoint or "/students/{studentId}" - return student at index
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // just index into the list ... keep it simple for now
+
+        return theStudents.get(studentId);
     }
 }
