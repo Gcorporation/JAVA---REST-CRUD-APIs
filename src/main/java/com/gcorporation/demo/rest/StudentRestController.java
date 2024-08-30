5eddacb452ff05100a5896ff54b6e78050c25952
Prabhjot Singh <prabhjotsingh394@gmail.com>
2024-08-30 11:05:09 +0530

    Spring Boot REST exception handling part 3 and 4

--- src/main/java/com/gcorporation/demo/rest/StudentRestController.java
+++ src/main/java/com/gcorporation/demo/rest/StudentRestController.java
@@ -70,4 +70,22 @@ public class StudentRestController {
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
+
+    // to catch any exception
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+
+        // create s StudentErrorResponse
+
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+
+    }
 }
